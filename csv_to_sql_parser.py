4d968b58fcbb3fb30e46a2ae6e083e97ab3e2f62
Maksim Gluwenko <maksim_gluwenko@epam.com>
2025-01-21 18:46:32 +0500

    Add csv to sql parser

---
+++ csv_to_sql_parser.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+def split_column_data(df, column_name):
+    df[column_name] = df[column_name].dropna().str.split(", ")
+    return df
+
+
+if __name__ == "__main__":
+    df = pd.read_csv("netflix.csv")
+    df["date_added"] = pd.to_datetime(df["date_added"], errors="coerce")
+    df["release_year"] = pd.to_datetime(df["date_added"], errors="coerce")
+    df = split_column_data(df, "cast")
+    df = split_column_data(df, "country")
+    df = split_column_data(df, "listed_in")
+    df = split_column_data(df, "director")
