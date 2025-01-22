8f8abc177f8700518fd44df56225b3323886b4cd
Maksim Gluwenko <maksim_gluwenko@epam.com>
2025-01-22 22:37:44 +0500

    Add array field type

--- app/db/models.py
+++ app/db/models.py
@@ -1,5 +1,5 @@
 import uuid
-from sqlalchemy import Column, Integer, String, Text, Date, Boolean
+from sqlalchemy import Column, Integer, String, Text, Date, Boolean, ARRAY
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.dialects.postgresql import UUID
 
@@ -12,14 +12,14 @@ class Show(Base):
     show_id = Column(Text, primary_key=True)
     type = Column(String(50))
     title = Column(String(200))
-    director = Column(Text)
-    cast = Column(Text)
-    country = Column(Text)
+    director = Column(ARRAY(String))
+    cast = Column(ARRAY(String))
+    country = Column(ARRAY(String))
     date_added = Column(Date)
     release_year = Column(Integer)
     rating = Column(String(10))
     duration = Column(String(20))
-    listed_in = Column(Text)
+    listed_in = Column(ARRAY(String))
     description = Column(Text)
 
 
