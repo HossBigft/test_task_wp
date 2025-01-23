23f7e4ca2e521e05880b8311beb24546f7bd0559
Maksim Gluwenko <maksim_gluwenko@epam.com>
2025-01-23 17:11:07 +0500

    Add flask cors and enable all cors

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager, create_access_token
+from flask_cors import CORS
+
+
 from app.config import settings
 from app.models import ShowSearchInput, LogInput
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = str(settings.SQLALCHEMY_DATABASE_URI)
-
+CORS(app)
 
 db = SQLAlchemy(app)
 
